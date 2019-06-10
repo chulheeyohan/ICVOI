fd6ba1ac8bd3eeeeb6d509448e3d4b6bcc22a7db
C Lee <32197811+chulheeyohan@users.noreply.github.com>
2019-06-10 14:55:44 -0700

    Update w2ICVOI.m

--- w2ICVOI.m
+++ w2ICVOI.m
@@ -1,23 +1,27 @@
+%Image Import
 file_info= spm_vol('w5104-2019.5.30.11.10_rsl.img');
->> imge_file_data = spm_read_vols(file_info);
->> temp = permute(imge_file_data,[2 1 3]);
->> Tx00 = temp(:,:,1);
->> imagesc(Tx00)
->> Tx35 = temp(:,:,35);
->> imagesc(Tx35)
->> axis equal
->> lTx35 = reshape(Tx35,95*79);
-??? Error using ==> reshape
-Size vector must have at least two elements.
+imge_file_data = spm_read_vols(file_info);
+temp = permute(imge_file_data,[2 1 3]);
+N = prod(size(temp));%517845
+temp_1D = reshape(temp,[N,1]);%temp in 1D
 
->> lTx35 = reshape(Tx35,[95*79 1]);
->> figure
->> histogram(lTx35)
-??? Undefined command/function 'histogram'.
+%Transaxial VINCI plane 034 and Coronal VINCI 047
+Tx35 = temp(:,:,35);
+Cor48 = temp(48,:,:);
+Cor48 = permute(Cor48,[2 3 1]); % this makes a 2D matrix
 
->> hist(lTx35)
->> lTx35_over_05=find(lTx35>0.5);
->> lTx35_over_05 = 
+%
+Yx = 95;
+Zx = 69;
+Tx35g = sum(Tx35);%column sum
+Cor48g = sum(Cor48,2)';%row sum transposed
 
->> Corr48 = temp(48,:,:);
->> Corr48 = permute(Corr48,[2 3 1]);
+x = [1:79];
+y = [1:95];
+z = [1:69];
+[X,Y,Z] = meshgrid(x,y,z);
+inside = find((X-39).^2/[(73-1)/2]^2 + (Y-48).^2/[(95-1)/2]^2 + (Z-35).^2/[(69-1)/2]^2 <1);
+
+mean(temp_1D(inside))
+std(temp_1D(inside))
+length(inside)
