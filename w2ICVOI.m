eb51947a50991ecea455caabecccb8c0e07869ab
C Lee <32197811+chulheeyohan@users.noreply.github.com>
2019-06-11 15:04:54 -0700

    Update w2ICVOI.m

--- w2ICVOI.m
+++ w2ICVOI.m
@@ -179,10 +179,10 @@ num_vxl = length(inside)
 s = pwd;
 file_info_ICVOI = file_info;
 s_slash = regexp(s,'\');
-patient = s(...
+person = s(...
   s_slash(max(find(s_slash-union(regexp(s,'F_'),regexp(s,'M_'))<0)))+1:...
   union(regexp(s,'F_'),regexp(s,'M_')));
-file_info_ICVOI.fname = [patient,'_ICVOI.img'];
+file_info_ICVOI.fname = [person,'_ICVOI.img'];
 file_info_ICVOI.descrip = 'spm - algebra';
 expression = ['i1/',num2str(ave_intensity)];
 Vo = spm_imcalc(file_info,file_info_ICVOI,expression);
